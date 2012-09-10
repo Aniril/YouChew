7288c52503742b215cea224b91c1c0dee1731822
James Conner Gray <james.gray-2@selu.edu>
2012-09-10 14:58:11 -0700

    Added Google Maps Test
    Added a view and a controller for testing Google Maps integration.

---
+++ YouChew/Controllers/ViewTestsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YouChew.Controllers
+{
+    public class ViewTestsController : Controller
+    {
+        //
+        // GET: /ViewTests/
+
+        public ActionResult MapsTest()
+        {
+            ViewBag.Message = "This is a page for testing things. Do not be afraid.";
+            
+            return View();
+        }
+
+    }
+}
