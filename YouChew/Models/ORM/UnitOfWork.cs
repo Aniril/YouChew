63c5d9098cdeb14ddfe991ba7670e1199e55db48
Sam Seghers <samuel.seghers@selu.edu>
2012-09-19 22:28:02 -0500

    Repository patterns and such fun

---
+++ YouChew/Models/ORM/UnitOfWork.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YouChew.Models.ORM
+{
+	public class UnitOfWork : IDisposable
+	{
+		private SiteContext context = new SiteContext();
+		private GenericRepository<User> userRepo;
+		private GenericRepository<Restaurant> restaurantRepo;
+		private GenericRepository<Critique> critiqueRepo;
+		//private Repository to implement <User> and <Role>
+
+		public GenericRepository<User> UserRepository
+		{
+			get
+			{
+				if (this.userRepo == null)
+				{
+					this.userRepo = new GenericRepository<User>(context);
+				}
+				return userRepo;
+			}
+		}
+
+		public GenericRepository<Restaurant> RestaurantRepository
+		{
+			get
+			{
+				if(this.restaurantRepo == null)
+				{
+					this.restaurantRepo = new GenericRepository<Restaurant>(context);
+				}
+				return restaurantRepo;
+			}
+		}
+
+		public GenericRepository<Critique> CritiqueRepository
+		{
+			get
+			{
+				if(this.critiqueRepo == null)
+				{
+					this.critiqueRepo = new GenericRepository<Critique>(context);
+				}
+				return critiqueRepo;
+			}
+		}
+
+		//other repo methods here
+
+		public void Save()
+		{
+			context.SaveChanges();
+		}
+
+		private bool disposed = false;
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if(!this.disposed)
+			{
+				if(disposing)
+				{
+					context.Dispose();
+				}
+			}
+			this.disposed = true;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
