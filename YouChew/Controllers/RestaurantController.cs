4c5c4cd0284ccfc5e8f04a0dbe6925e25c099449
James Conner Gray <james.gray-2@selu.edu>
2012-09-24 23:12:05 -0700

    Restaurant Page Rough Draft
    A very early draft for the restaurant view page. Still need to add some
    styling, cascading critiques and having it pull actual data.

---
+++ YouChew/Controllers/RestaurantController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YouChew.Models;
+
+namespace YouChew.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        //
+        // GET: /Restaurant/
+
+        public ActionResult Restaurant()
+        {
+            Restaurant tempRes = new Restaurant();
+            tempRes.name = "Kentucky Fried Chicken";
+            tempRes.phone = "(555)555-5555";
+            tempRes.cuisine = "Delicious Chicken";
+
+            return View("Restaurant", tempRes);
+        }
+
+    }
+}
