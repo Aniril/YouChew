9a1dda4e4391675a7bef1efd8b76accf946d5ec5
Jason V <aniril@gmail.com>
2012-10-17 00:13:11 -0500

    Small fix
    Facebook button is now using the xfbml button, and because of that it
    all just got super easy in the javascript.

--- YouChew/Scripts/Facebook.js
+++ YouChew/Scripts/Facebook.js
@@ -8,45 +8,23 @@
             oauth: true
         });
 
-        function authStuff(response) {
-            var button = document.getElementById('fb-auth');
-            //assuming logged in    
+        FB.Event.subscribe('auth.login', function(response) {  
             if (response.authResponse) {
-                console.log('Welcome! Fetching your information...');
-                var userInfo = document.getElementById('user-info');
                 FB.api('/me', function (response) {
-                    userInfo.innerHTML = 'Welcome, ' + response.name + ' <img src="https://graph.facebook.com/'
+                    var userInfo = document.getElementById('user-info');
+                    userInfo.innerHTML = response.name
+                    + ' <img src="https://graph.facebook.com/'
                     + response.id + '/picture">';
-                    button.innerHTML = 'Logout';
                 });
-                //onClick to logout
-                button.onclick = function () {
-                    FB.logout(function (response) {
-                        var userInfo = document.getElementById('user-info');
-                        userInfo.innerHTML = "";
-                    });
-                };
             } else {
-                button.innerHTML = 'Login';
-
-                button.onclick = function () {
-                    FB.login(function (response) {
-                        if (response.authResponse) {
-                            FB.api('/me', function (response) {
-                                var userInfo = document.getElementById('user-info');
-                                userInfo.innerHTML = response.name
-                                + ' <img src="https://graph.facebook.com/'
-                                + response.id + '/picture">';
-                            });
-                        } else {
-                            console.log('User cancelled login or did not fully authorize.');
-                        }
-                    }, { scope: 'email,publish_stream' });
-                }
+                console.log('User cancelled login or did not fully authorize.');
             }
-        }
-        FB.getLoginStatus(authStuff);
-        FB.Event.subscribe('auth.statusChange', authStuff);
+        });
+
+        FB.Event.subscribe('auth.logout', function(response) {
+            var userInfo = document.getElementById('user-info');
+            userInfo.innerHTML = "";
+        });  
     };
 
     (function () {
@@ -56,59 +34,3 @@
         document.getElementById('fb-root').appendChild(e);
     } ());
 }
-
-/*
-function InitialiseFacebook(appId) {
-
-    window.fbAsyncInit = function () {
-        FB.init({
-            appId: appId,
-            status: true,
-            cookie: true,
-            xfbml: true
-        });
-
-        FB.Event.subscribe('auth.login', function (response) {
-            var credentials = { FBid: response.authResponse.userID, accessToken: response.authResponse.accessToken };
-            SubmitLogin(credentials);
-        });
-
-        FB.getLoginStatus(function (response) {
-            if (response.status === 'connected') {
-                console.log('user is logged into fb');
-            }
-            else if (response.status === 'not_authorized') { console.log('user is not authorised'); }
-            else { console.log('user is not conntected to facebook'); }
-
-        });
-
-        function SubmitLogin(credentials) {
-            $.ajax({
-                url: "/account/facebooklogin",
-                type: "POST",
-                data: credentials,
-                error: function () {
-                    console.log('error logging in to your facebook account.');
-                },
-                success: function () {
-                    window.location.reload();
-                }
-            });
-        }
-
-    };
-
-    (function (d) {
-        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-        if (d.getElementById(id)) {
-            return;
-        }
-        js = d.createElement('script');
-        js.id = id;
-        js.async = true;
-        js.src = "//connect.facebook.net/en_US/all.js";
-        ref.parentNode.insertBefore(js, ref);
-    } (document));
-
-}
-*/
